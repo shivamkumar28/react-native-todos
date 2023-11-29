fba650bd337b2e95e040ca6d03b82f8bc89c2506
shivamkumar28 <skp42574@gmail.com>
2023-11-29 21:22:43 +0530

    setup redux toolkit

--- App.tsx
+++ App.tsx
@@ -10,19 +10,23 @@ import {
 } from 'react-native';
 import InitialNavigation from './src/navigation';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
+import store from './src/redux';
 
 function App(): JSX.Element {
 
   return (
-    <SafeAreaProvider>
-      <StatusBar
-        backgroundColor={'white'}
-        barStyle={'dark-content'}
-      />
-      <View style={{ flex: 1 }}>
-        <InitialNavigation />
-      </View>
-    </SafeAreaProvider>
+    <Provider store={store} >
+      <SafeAreaProvider>
+        <StatusBar
+          backgroundColor={'white'}
+          barStyle={'dark-content'}
+        />
+        <View style={{ flex: 1 }}>
+          <InitialNavigation />
+        </View>
+      </SafeAreaProvider>
+    </Provider>
   );
 }
 
