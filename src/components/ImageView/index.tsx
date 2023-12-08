1f523822d782a794de2bd5aa6d8396f453eb593b
shivamkumar28 <skp42574@gmail.com>
2023-12-08 07:00:42 +0530

    remove unneccesary code

--- src/components/ImageView/index.tsx
+++ src/components/ImageView/index.tsx
@@ -1,28 +1,8 @@
 import React from 'react';
-import { Platform, UIManager, requireNativeComponent } from 'react-native';
+import { View } from 'react-native';
 
-
-const ComponentName = 'MYRCTImageView';
-
-const LINKING_ERROR =
-    `The package 'RNTRecentGalleryiOS' doesn't seem to be linked. Make sure: \n\n` +
-    Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
-    '- You rebuilt the app after installing the package\n' +
-    '- You are not using Expo Go\n';
-
-interface RNImageViewProps {
+const ImageView = () => {
+    return <View></View>
 }
 
-const RCTCustomView =
-    UIManager.getViewManagerConfig(ComponentName) != null
-        ? requireNativeComponent<RNImageViewProps>(ComponentName)
-        : () => {
-            throw new Error(LINKING_ERROR);
-        };
-
-class MYRCTImageView extends React.PureComponent<RNImageViewProps> {
-    render() {
-        return <RCTCustomView {...this.props} />;
-    }
-}
-export default MYRCTImageView;
+export default ImageView;
--- src/modules/dashboard/index.tsx
+++ src/modules/dashboard/index.tsx
@@ -1,11 +1,11 @@
 import { Button, View } from "react-native"
 import { styles } from "./style"
-import MYRCTImageView from "../../components/ImageView"
+
 
 const Dashboard = () => {
     return <View style={styles.container}>
         <Button title={'Add'} />
-        <MYRCTImageView />
+
     </View>
 }
 
