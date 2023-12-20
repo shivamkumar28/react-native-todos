90aa895ea1361d63e8153b5c14e4e7b7d255eec7
shivamkumar28 <skp42574@gmail.com>
2023-12-20 07:05:29 +0530

    feat: integrste netfox ios

--- ios/todos/AppDelegate.mm
+++ ios/todos/AppDelegate.mm
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import "NetFox/Netfox-Swift.h"
 
 @implementation AppDelegate
 
@@ -10,6 +11,12 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+
+
+#if DEBUG
+  [NFX.sharedInstance start];
+  // [NFX.sharedInstance setGesture custom]
+#endif
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
--- src/modules/dashboard/index.tsx
+++ src/modules/dashboard/index.tsx
@@ -1,11 +1,28 @@
 import { Button, View } from "react-native"
 import { styles } from "./style"
-
+import axios from 'axios';
 
 const Dashboard = () => {
-    return <View style={styles.container}>
-        <Button title={'Add'} />
 
+    const callPostApi = () => {
+        axios.get('https://reqres.in/api/users?page=2')
+            .then((val: any) => {
+                console.log('value---', val.data.data)
+            })
+            .catch((e: any) => {
+                console.log('error-', e)
+            })
+    }
+
+    const openDebugger = () => {
+
+    }
+
+
+    return <View style={styles.container}>
+        <Button title={'Add'} onPress={callPostApi} />
+
+        <Button title={'Open'} onPress={openDebugger} />
     </View>
 }
 
